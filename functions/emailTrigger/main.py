e8884fc4e69c7395ddd4af2407d8d8cefa539b5b
Aman <amansaini7999@gmail.com>
2021-05-24 01:32:53 +0530

    Added email trigger working

--- functions/emailTrigger/main.py
+++ functions/emailTrigger/main.py
@@ -1,25 +1,19 @@
 def sendCustomEmail(email, msg):
     from config import senderEmail, senderPassword
-    import smtplib
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-
-        print(senderEmail)
+    import smtplib, ssl
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(senderEmail, senderPassword)
-
         smtp.sendmail(senderEmail, email, msg)
-
     return True
 
 def emailTrigger(data, context):
     if 'email' in data['updateMask']['fieldPaths']:
         email = data['value']['fields']['email']['stringValue']
         subject = 'Notification from covid help'
-        body = "Dear XYZ\n\nThanks for registering your email with our covid help platform\n\n Regards,\nTeam covid help"
-        msg = f'Subject: {subject}\n\n{body}'
-        # sendCustomEmail(email, msg)
+        body = "Dear XYZ,\nThanks for registering your email with our covid help platform.\n\n Regards,\nTeam covid help"
+        msg = f'To:{email}\nSubject: {subject}\n\n{body}'
+        sendCustomEmail(email, msg)
         return True
 
     else:
