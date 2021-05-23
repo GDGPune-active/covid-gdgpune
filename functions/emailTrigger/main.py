56f28e172283d07aae8122d23fc23624d8dd4209
Aman <amansaini7999@gmail.com>
2021-05-23 18:34:36 +0530

    Email trigger part added

---
+++ functions/emailTrigger/main.py
@@ -0,0 +1,26 @@
+def sendCustomEmail(email, msg):
+    from config import senderEmail, senderPassword
+    import smtplib
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        print(senderEmail)
+        smtp.login(senderEmail, senderPassword)
+
+        smtp.sendmail('amansaini17675@gmail.com', 'amansaini7999@gmail.com', msg)
+
+    return True
+
+def emailTrigger(data, context):
+    if 'email' in data['updateMask']['fieldPaths']:
+        email = data['value']['fields']['email']['stringValue']
+        subject = 'Notification from covid help'
+        body = "Dear XYZ\n\nThanks for registering your email with our covid help platform\n\n Regards,\nTeam covid help"
+        msg = f'Subject: {subject}\n\n{body}'
+        # sendCustomEmail(email, msg)
+        return True
+
+    else:
+        return False
