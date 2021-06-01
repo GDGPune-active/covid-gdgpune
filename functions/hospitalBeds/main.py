32f4ca23e880e5c80ce648cd4d6362d0fb391892
Aman <amansaini7999@gmail.com>
2021-06-01 23:52:19 +0530

    Added function to add total values

--- functions/hospitalBeds/main.py
+++ functions/hospitalBeds/main.py
@@ -18,13 +18,77 @@ def hospitalBeds(data, context):
         uid2 = context[-1]
 
         doc_ref = db.collection(u'userProfile').document(uid1).collection(u'hospitalBeds').document(uid2)
+        bedType = data['value']['fields']['bedType']['stringValue']
+        vacantBeds = data['value']['fields']['vacantNo']['stringValue']
 
         if(data['value']['fields']['verified']['stringValue']=='Yes'):
             doc_ref.update({
                 u'date': firestore.SERVER_TIMESTAMP
                 })
+
+            # ADD value
+            total_ref = db.collection(u'totalData').document('totalDataforDashboard')
+            total = total_ref.to_dict()
+
+            total_ref.update({
+                'totalBeds': total['totalBeds']+vacantBeds
+                })
+
+            if(bedType == 'Without Oxygen'):
+                total_ref.update({
+                'withoutOxygen': total['withoutOxygen']+vacantBeds
+                })
+
+            elif(bedType == 'With Oxygen'):
+                total_ref.update({
+                'withOxygen': total['withOxygen']+vacantBeds
+                })
+
+            elif(bedType == 'ICU with ventilator'):
+                total_ref.update({
+                'ICUwithVentilator': total['ICUwithVentilator']+vacantBeds
+                })
+
+            elif(bedType == 'ICU without ventilator'):
+                total_ref.update({
+                'ICUwithoutVentilator': total['ICUwithoutVentilator']+vacantBeds
+                })
+
         else:
             doc_ref.update({
                 u'date': firestore.DELETE_FIELD
                 })
-    return True
+            
+            if 'verified' in data['oldValue']['fields']:
+                
+                #subtract
+                total_ref = db.collection(u'totalData').document('totalDataforDashboard')
+                total = total_ref.to_dict()
+
+                total_ref.update({
+                    'totalBeds': total['totalBeds']-vacantBeds
+                    })
+
+                if(bedType == 'Without Oxygen'):
+                    total_ref.update({
+                    'withoutOxygen': total['withoutOxygen']-vacantBeds
+                    })
+
+                elif(bedType == 'With Oxygen'):
+                    total_ref.update({
+                    'withOxygen': total['withOxygen']-vacantBeds
+                    })
+
+                elif(bedType == 'ICU with ventilator'):
+                    total_ref.update({
+                    'ICUwithVentilator': total['ICUwithVentilator']-vacantBeds
+                    })
+
+                elif(bedType == 'ICU without ventilator'):
+                    total_ref.update({
+                    'ICUwithoutVentilator': total['ICUwithoutVentilator']-vacantBeds
+                    })
+        return True
+
+    else:
+        return False
